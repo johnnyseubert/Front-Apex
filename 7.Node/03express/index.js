87736374ece61f56a4dfcb6221810b11b7de10c8
Johnny Blasius Seubert <jbseubert@gmail.com>
2020-09-27 19:27:49 -0300

    Alterando Arquivos CSS 27/09

--- 7.Node/03express/index.js
+++ 7.Node/03express/index.js
@@ -9,8 +9,8 @@ app.get('/', (req, res) => {  //Quando ta limpo "localhost:8080" ele faz a situa
     res.send("<h1>Aprendendo Node.js</h1>");
 });
 
-app.get('/sobre/:nome/:idade', (obter, enviar) => {  //Quando ta "localhost:8080/sobre" ele faz a situação abaixo
-    enviar.send("<h1>Olá " + obter.params.nome + " você tem " + obter.params.idade + " anos</h1>");
+app.get('/sobre/:nome/:idade', (req, res) => {  //Quando ta "localhost:8080/sobre" ele faz a situação abaixo
+    res.send("<h1>Olá " + req.params.nome + " você tem " + req.params.idade + " anos</h1>");
 });
 
 // Servidor
